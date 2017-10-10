f682ae0ff51180d1a9c234d8b01c886d8532df1b
Ivan Kostadinović <ivan.k@vivifyideas.com>
2017-10-10 20:57:07 +0200

    Implemented sectors

--- src/app/animal-list/animal-list.component.ts
+++ src/app/animal-list/animal-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Animal } from './animal';
+import { Sector } from './sector';
 
 @Component({
   selector: 'app-animal-list',
@@ -14,13 +15,18 @@ export class AnimalListComponent implements OnInit {
   ngOnInit() {
   }
 
+  sector1 = new Sector('One', 'land');
+  sector2 = new Sector('Two', 'water');
+  sector3 = new Sector('Three', 'mud');
+  sector4 = new Sector('Four', 'sand');
+
   animals:Array<Animal> = [
 
-      new Animal("cow", "redcow", "2017"),
-      new Animal("dog", "jackie", "1999"),
-      new Animal("cat", "meow", "1998"),
-      new Animal("tiger", "sheerkhan", "2000"),
-      new Animal("lion", "king")
+      new Animal("cow", "redcow", this.sector1, "2017"),
+      new Animal("dog", "jackie", this.sector2,"1999"),
+      new Animal("cat", "meow", this.sector3,"1998"),
+      new Animal("tiger", "sheerkhan", this.sector4,"2000"),
+      new Animal("lion", "king", this.sector3)
 
   ]
 
@@ -33,10 +39,10 @@ export class AnimalListComponent implements OnInit {
       this.animals.unshift(row[0]);
   }
 
-  newAnimal:Animal = new Animal('', '', '');
+  newAnimal:Animal = new Animal('', '', this.sector1, '');
 
   submitAnimal() {
       this.animals.push(this.newAnimal);
-      this.newAnimal = new Animal('', '', '');
+      this.newAnimal = new Animal('', '', this.sector1, '');
   }
 }
--- src/app/animal-list/animal.ts
+++ src/app/animal-list/animal.ts
@@ -1,16 +1,19 @@
-
+import { Sector } from './sector';
 
 export class Animal {
 
     species:string;
     name:string;
     dateOfBirth:string;
+    sector:Sector;
 
-    constructor(species:string, name:string, dateOfBirth:string = "") {
+    constructor(species:string, name:string, sector:Sector, dateOfBirth:string = "") {
 
         this.species = species;
         this.name = name;
+        this.sector = sector;
         this.dateOfBirth = dateOfBirth;
+
 
     }
 
