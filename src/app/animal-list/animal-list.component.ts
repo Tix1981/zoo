298059f6b0b01dd87d57199286839aad8d589804
Ivan Kostadinović <ivan.k@vivifyideas.com>
2017-10-10 21:33:33 +0200

    show animals in sector functionalitu

--- src/app/animal-list/animal-list.component.ts
+++ src/app/animal-list/animal-list.component.ts
@@ -15,18 +15,21 @@ export class AnimalListComponent implements OnInit {
   ngOnInit() {
   }
 
-  sector1 = new Sector('One', 'land');
-  sector2 = new Sector('Two', 'water');
-  sector3 = new Sector('Three', 'mud');
-  sector4 = new Sector('Four', 'sand');
+  sectors:Array<Sector> = [
+      new Sector('One', 'land'),
+      new Sector('Two', 'water'),
+      new Sector('Three', 'land'),
+      new Sector('Four', 'water'),
+  ]
+
 
   animals:Array<Animal> = [
 
-      new Animal("cow", "redcow", this.sector1, "2017"),
-      new Animal("dog", "jackie", this.sector2,"1999"),
-      new Animal("cat", "meow", this.sector3,"1998"),
-      new Animal("tiger", "sheerkhan", this.sector4,"2000"),
-      new Animal("lion", "king", this.sector3)
+      new Animal("cow", "redcow", this.sectors[0], "2017"),
+      new Animal("dog", "jackie", this.sectors[1],"1999"),
+      new Animal("cat", "meow", this.sectors[2],"1998"),
+      new Animal("tiger", "sheerkhan", this.sectors[3],"2000"),
+      new Animal("lion", "king", this.sectors[2])
 
   ]
 
@@ -39,10 +42,21 @@ export class AnimalListComponent implements OnInit {
       this.animals.unshift(row[0]);
   }
 
-  newAnimal:Animal = new Animal('', '', this.sector1, '');
+  newAnimal:Animal = new Animal('', '', this.sectors[0], '');
 
   submitAnimal() {
       this.animals.push(this.newAnimal);
-      this.newAnimal = new Animal('', '', this.sector1, '');
+      this.newAnimal = new Animal('', '', this.sectors[0], '');
+  }
+
+  showSectorAnimals(index) {
+      let sectorAnimals:Array<string> = [];
+
+      for(var i = 0; i<this.animals.length; i++) {
+          if(this.animals[i].sector.name === this.sectors[index].name) {
+              sectorAnimals.push(this.animals[i].name);
+          }
+      }
+      alert(sectorAnimals);
   }
 }
