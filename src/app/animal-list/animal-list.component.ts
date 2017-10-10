9fad5218fcae462e33c3c0bff994105a69928541
Ivan Kostadinović <ivan.k@vivifyideas.com>
2017-10-10 19:16:48 +0200

    click method for deleting rows implemented

--- src/app/animal-list/animal-list.component.ts
+++ src/app/animal-list/animal-list.component.ts
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AnimalListComponent implements OnInit {
 
+  deleteRow(index) {
+      this.animals.splice(index, 1);
+  }
+
   constructor() { }
 
   ngOnInit() {
